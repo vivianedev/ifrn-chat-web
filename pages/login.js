cf7801083575375cd401534c3f2be37efaa6f30f
vivianedev <vivianethedev@gmail.com>
2022-05-31 15:46:24 -0300

    adicionando botão de voltar em login.js

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {AppContainer} from '../src/components/AppContainerComponent';
-import {AppButton} from '../src/components/AppButtonComponent';
-import {AppInput} from '../src/components/AppInputComponent';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { AppButton,AppContainer, AppInput } from '../src/components';
 
 const Login = props => {
+  const router = useRouter();
+
   return (
-		<AppContainer>
-			<AppInput
-				title="apelido ou email do usuário"
-				label="Usuário:"
-				type="text"
-			/>
-			<AppInput 
-				label="Senha:" 
-				type="password" 
-				title="senha do usuário" 
-			/>
-			<AppButton title="Entrar" />
-		</AppContainer>
-	);
+    <div>
+      <Head>
+        <title>página de login</title>
+      </Head>
+      <main>
+        <nav>
+          <p>Chat de Infoweb</p>
+          <button onClick={() => router.back()}>voltar</button>
+        </nav>
+        <AppContainer>
+          <AppInput label="Usuário" title="apelido ou email do usuário"/>
+          <AppInput label="Senha" type="password" />
+          <AppButton title="Clique aqui para entrar no chat" label="Entrar no chat" />
+        </AppContainer>
+      </main>
+      </div>
+  );
 };
 
 export default Login;

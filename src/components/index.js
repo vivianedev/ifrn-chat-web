ad800e6567b7f5fb6d295f07627a3fbd197aadc6
vivianedev <vivianethedev@gmail.com>
2022-05-31 15:50:41 -0300

    update

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import {AppButton} from '../components/AppButtonComponent';
+import {AppContainer} from '../components/AppContainerComponent';
+import {AppInput} from '../components/AppInputComponent';
+
+export {AppButton, AppContainer, AppInput};

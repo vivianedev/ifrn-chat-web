fb9928526ae2d6158a0750ff9b581e3f62e80570
vivianedev <vivianethedev@gmail.com>
2022-07-26 22:05:48 -0300

    modificando AppButton para uso de estado de componente

--- src/components/AppButtonComponent.js
+++ src/components/AppButtonComponent.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import Button from "@mui/material/Button";
 
-export const AppButton = props => (
-  <div>
-    <input type="submit" title={props.title} />
-  </div>
-);
+export const AppButton = (props) => {
+  let color_button = props.color ?? "primary";
+  let handle_click = props.onClick ?? (() => null);
+  let title_button = props.title ?? "unamed";
+
+  return (
+    <Button
+      color={color_button}
+      onClick={handle_click}
+      title={title_button}
+      {...props}
+    >
+      {props.label}
+    </Button>
+  );
+};

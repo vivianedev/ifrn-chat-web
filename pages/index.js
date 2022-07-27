c1978c7db9d3a3890102c5e8f00dae88b777d7ca
vivianedev <vivianethedev@gmail.com>
2022-07-26 22:13:11 -0300

    Adicionando lógica nos botões login/logout na página de home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import { Toolbar } from "@mui/material";
 import { IconButton } from "@mui/material";
 import { AppBar } from "@mui/material";
-import { Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Head from "next/head";
+import { Typography } from "@mui/material";
 import { AppButton } from "../src/components";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const AppLoginLogoutButton = (props) => {
+   if (props.login) {
+      return <AppButton color="inherit" label="Login" href="/login" />;
+   } else {
+      return (
+         <AppButton
+            color="inherit"
+            label="logout"
+            onClick={(e) => null}
+         />
+      );
+   }
+};
 
 const AppNavBar = (props) => {
    return (
@@ -22,9 +38,9 @@ const AppNavBar = (props) => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                {props.title}
             </Typography>
-            <AppButton color="inherit" label="Login" href="/login" />
-            <AppButton color="inherit" label="Logout" href="/logout" />         
-				 </Toolbar>
+            <AppLoginLogoutButton login/>
+            <AppLoginLogoutButton />
+         </Toolbar>
       </AppBar>
    );
 };
@@ -41,6 +57,8 @@ export default function Home() {
          <main>
             <AppNavBar
                title="Chat de Infoweb"
+               userLogged={isUserLogged()}
+               logoutFunction={(e) => setSessionId("")}
             />
             <h1>Landing page do chat de Infoweb</h1>
          </main>

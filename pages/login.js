340d29cb0036bfa993db7e28535aa59fa86656c0
vivianedev <vivianethedev@gmail.com>
2022-07-26 22:07:28 -0300

    modificando página de login

--- pages/login.js
+++ pages/login.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { AppButton,AppContainer, AppInput } from '../src/components';
+import {useRouter} from 'next/router';
+import {AppButton, AppContainer, AppInput} from '../src/components';
 
 const Login = props => {
-  const router = useRouter();
+  const router = useRouter ();
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  }
 
   return (
     <div>
       <Head>
-        <title>página de login</title>
+        <title>Infoweb chat - Login</title>
       </Head>
       <main>
-        <nav>
-          <p>Chat de Infoweb</p>
-          <button onClick={() => router.back()}>voltar</button>
-        </nav>
         <AppContainer>
-          <AppInput label="Usuário" title="apelido ou email do usuário"/>
-          <AppInput label="Senha" type="password" />
-          <AppButton title="Clique aqui para entrar no chat" label="Entrar no chat" />
+          <AppInput 
+            label="Usuário" 
+            title="apelido ou email do usuário" 
+            value={name} 
+            onChange={handleChangeName}
+          />
+          <AppInput 
+            label="Senha" 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <AppButton
+            onClick={event => router.back ()}
+            label="voltar"
+            color="secondary"
+          />
+          <AppButton
+            title="Clique aqui para entrar no chat"
+            label="Entrar no chat"
+            variant='contained'
+            disabled={!(!!name && !!password)}
+          />
         </AppContainer>
       </main>
-      </div>
+    </div>
   );
 };
 
